18eec190175983de8d31f8a0ed54f9818e4c72c6
FOX11097 <timothy.privat@gmx.de>
2026-02-09 13:21:43 +0100

    Data_Penguins Wrote Function to plot body mass and bill length for any species

---
+++ Penguins_Function.R
@@ -0,0 +1,21 @@
+library(palmerpenguins)
+data("penguins")
+view(penguins)
+
+library(tidyverse)
+library(palmerpenguins)
+
+#Function that creates a Plot for any species found in the 
+#dataset with Bill length (BL) against body mass (BM)
+penguin_BL_BM <- function(.data, species){
+  .data |>
+    filter(species == {{species}}) |>
+    ggplot(aes(x = body_mass_g, y = bill_length_mm)) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    labs(x = "Body mass g", y = "Bill length mm", title = {{species}})
+}
+
+penguin_BL_BM(penguins, "Gentoo")
+
+penguin_BL_BM(penguins, "Adelie")
