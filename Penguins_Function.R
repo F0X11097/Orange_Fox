70e6382f987100a07daf60c8281e26f8710a69b7
FOX11097 <timothy.privat@gmx.de>
2026-02-09 13:37:16 +0100

    Added copy of function

--- Penguins_Function.R
+++ Penguins_Function.R
@@ -19,3 +19,13 @@ penguin_BL_BM <- function(.data, species){
 penguin_BL_BM(penguins, "Gentoo")
 
 penguin_BL_BM(penguins, "Adelie")
+
+#Function that creates a Plot for any species found in the 
+#dataset with bill_depth (BL) against body mass (BM)
+penguin_BL_BM <- function(.data, species){
+  .data |>
+    filter(species == {{species}}) |>
+    ggplot(aes(x = body_mass_g, y = bill_length_mm)) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    labs(x = "Body mass g", y = "Bill length mm", title = {{species}})
